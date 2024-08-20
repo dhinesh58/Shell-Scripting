ea9c87cb8d1624911315ae77a44849d879787136
rdhinesh58 <rdhinesh58@gmail.com>
2024-08-20 16:48:32 +0530

    first commit

---
+++ Azure_resources_list.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+set -x 
+####################################################################################
+# Script to automate the process of listing all the resources in azure account
+# Author : Dhinesh/ Infra Team 
+# Version : V0.0.1
+# Date : 16-Aug-2024
+# 
